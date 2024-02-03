0e7390b1b33d87eb85acee8513d41341ca13b710
Boban Kilvin <btrajcevski10@gmail.com>
2024-02-03 13:50:53 -0500

    Center smaller images to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,9 @@ import "./Card.style.css";
 export default function Card({ card, onSelect }) {
   return (
     <div className="card" onClick={() => onSelect(card)}>
-      <img src={card.imgUrl} alt={card.name} />
+      <div className="img-container">
+        <img src={card.imgUrl} alt={card.name} />
+      </div>
       <span className="card-name">{card.name}</span>
     </div>
   );
